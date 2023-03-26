6dd0e4383936662dca8fb0cfbbd9f2e38e245d47
Mesut Kızılay <mesutkizilay3868@gmail.com>
2023-03-26 15:41:13 +0300

    Abstract and concrete files were created

---
+++ Entities/Concrete/User.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class User:IEntity
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        string Surname { get; set; }
+        string PhoneNumber { get; set; }
+        string Email { get; set; }
+        
+    }
+}
