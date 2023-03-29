4974ee5cbb443bc66a969c3b038185080cc211d4
Mesut Kızılay <mesutkizilay3868@gmail.com>
2023-03-29 21:20:04 +0300

    Classes of DataAccess were created

---
+++ DataAccess/Concrete/EarthquakeContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class EarthquakeContext:DbContext
+    {
+        protected static void OnConfigure(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseSqlServer("Server = DESKTOP - 8DD9NIJ; Database = Northwind; Trusted_Connection = true");
+
+           
+    } public DbSet<ColdVictim> ColdVictims { get; set; }
+    }
+}
