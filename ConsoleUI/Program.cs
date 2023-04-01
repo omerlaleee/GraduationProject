5527a798bad2ee85c96691f862cf92ded62395f6
Mesut Kızılay <mesutkizilay3868@gmail.com>
2023-04-01 16:56:21 +0300

    API layer were created

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             VictimManager victimManager = new VictimManager(new EfVictimDal());
-            Victim victim = new Victim { Name = "A", Surname = "A", Email = "A", PhoneNumber = "0", NumberOfPeople = 1, Address = "A", DetailedAddress = "A", Urgency = "A", StatusInformation = "A", MapsAddress = "A" };
+            Victim victim = new Victim { Name = "A", Surname = "A", Email = "A", PhoneNumber = "0", NumberOfPeople = 1, Address = "A", DetailedAddress = "A", Urgency = "A", StatusInformation = "A", MapsAddress = "A",VictimType="A" };
 
             var result1 = victimManager.Add(victim);
 
